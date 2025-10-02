592f53121febc63609dfe4c1325d34e3d7149a1f
Erik Kemper <e.h.m.kemper@erasmusmc.nl>
2025-10-02 16:25:04 +0200

    docstyle fixes in models.py

--- inflammation/models.py
+++ inflammation/models.py
@@ -14,21 +14,34 @@ def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
+    :returns: A 2D numpy array of inflammation data
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: A 2D numpy array of inflammation data
+    :returns: A 1D numpy array of daily mean inflammation values
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: A 2D numpy array of inflammation data
+    :returns: A 1D numpy array of daily max inflammation values
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+
+    :param data: A 2D numpy array of inflammation data
+    :returns: A 1D numpy array of daily min inflammation values
+    """
     return np.min(data, axis=0)
 
