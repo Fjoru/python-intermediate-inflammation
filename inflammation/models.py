6167b2aaa9bdcf027db8646ea478d993d10ac2fa
Erik Kemper <e.h.m.kemper@erasmusmc.nl>
2025-10-07 17:12:19 +0200

    add test debugging

--- inflammation/models.py
+++ inflammation/models.py
@@ -44,3 +44,9 @@ def daily_min(data):
     :returns: A 1D numpy array of daily min inflammation values
     """
     return np.min(data, axis=0)
+
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max = np.max(data, axis=0)
+    return data / max[:, np.newaxis]
