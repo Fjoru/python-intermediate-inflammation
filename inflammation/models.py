e41d2e21130e14f2731ea64cb290f2b5dc29acf4
Erik Kemper <e.h.m.kemper@erasmusmc.nl>
2025-10-10 10:16:36 +0200

    bug fix

--- inflammation/models.py
+++ inflammation/models.py
@@ -54,12 +54,15 @@ def patient_normalise(data: np.ndarray) -> np.ndarray:
     :param data: A 2D numpy array of inflammation data
     :returns: A 2D numpy array of normalised inflammation data
     """
+    if not isinstance(data, np.ndarray):
+        raise TypeError("Input data must be a numpy array")
+
     if np.any(data < 0):
         raise ValueError("Inflammation values should not be negative")
-    
-    max = np.max(data, axis=1)
+
+    max_data = np.max(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
